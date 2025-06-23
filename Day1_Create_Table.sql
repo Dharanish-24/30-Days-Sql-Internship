f2b3b0a46114719727b76f3b85107c0ca0e7b096
Dharanish-24 <dharanishbaabu@gmail.com>
2025-06-23 17:23:15 +0530

    feat : Create Table

--- Day1_Create_Table.sql
+++ Day1_Create_Table.sql
@@ -1,13 +1,22 @@
 --Doctors Table
-CREATE TABLE doctors (
-    doctor_id PRIMARY KEY,
+CREATE TABLE doctor (
+    doctor_id SERIAL PRIMARY KEY,
     doctor_name VARCHAR(100),
     specialty VARCHAR(110)
 );
 -- Patients Table 
-CREATE TABLE patients (
-    patient_id PRIMARY KEY,
+CREATE TABLE patient (
+    patient_id SERIAL PRIMARY KEY ,
     patient_name VARCHAR(100),
     age INT,
     phone VARCHAR(15)
+);
+--appointments
+CREATE TABLE appointment (
+    appointment_id SERIAL PRIMARY KEY,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATE,
+    FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
 );
