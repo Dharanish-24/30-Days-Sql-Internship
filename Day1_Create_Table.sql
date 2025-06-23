df486aa6e9aac7eb4d6c27f6f008cd086b01df38
Dharanish-24 <dharanishbaabu@gmail.com>
2025-06-23 17:09:04 +0530

    feat : Create Table

--- Day1_Create_Table.sql
+++ Day1_Create_Table.sql
@@ -1,5 +1,13 @@
+--Doctors Table
 CREATE TABLE doctors (
-    doctor_id  PRIMARY KEY,
+    doctor_id PRIMARY KEY,
     doctor_name VARCHAR(100),
     specialty VARCHAR(110)
+);
+-- Patients Table 
+CREATE TABLE patients (
+    patient_id PRIMARY KEY,
+    patient_name VARCHAR(100),
+    age INT,
+    phone VARCHAR(15)
 );
